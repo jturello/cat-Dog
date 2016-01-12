65d8a9cfb33d0625adf5f93441febbc59f9e0310
Brianna Popson and James Turello <jturello@yahoo.com>
2016-01-12 10:15:36 -0800

    Add button responses with text styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   $("button#cat").click(function() {
-    $("img#cat").after("<p>Hello!</p>");
+    $("img#cat").after('<p class="Meow init">Meeoooww!</p>');
+    $("img#dog").after('<p class="Scooby">Ruhroh!!</p>');
     });
 
-    $("button#cat").click(function() {
-      $("ul").prepend("<li>Hello!</li>");
+  $("button#dog").click(function() {
+    $("img#dog").after('<p class="Arf init">Arf arf!</p>');
+    $("img#cat").after('<p class="Garfield">Hisssssss</p>');
     });
 
   });
